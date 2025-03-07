feb5c1dbe688e3c2b543924d177d1330d965768f
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2025-03-07 12:59:13 -0300

    sketch.js

--- sketch.js
+++ sketch.js
@@ -3,9 +3,8 @@ var weight = [35,38,40,45];
 
 function setup() {
   createCanvas(400,400);
-
-  var sum = weight[0] + weight[1] + weight[2] + weight[3];
-  var average = sum/weight.length;
+  //faça a conta aqui
+  
   console.log(average);
 
 }
